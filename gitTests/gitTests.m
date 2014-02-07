b329418083cb658c32ba4b667d9daf49583ab310
grharon <grharon@gmail.con>
2014-02-07 11:08:59 +0800

    Initial Commit

---
+++ gitTests/gitTests.m
@@ -0,0 +1,34 @@
+//
+//  gitTests.m
+//  gitTests
+//
+//  Created by grharon on 2/7/14.
+//  Copyright (c) 2014 limalogy. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface gitTests : XCTestCase
+
+@end
+
+@implementation gitTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
